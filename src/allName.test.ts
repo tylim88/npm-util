94012683d55af1dbed972f3de04eca51dee484b9
tylim <limteikyee@gmail.com>
2022-02-13 18:52:26 +0800

    for circle ci

--- src/allName.test.ts
+++ src/allName.test.ts
@@ -7,6 +7,7 @@ describe('test all Name', () => {
 		await syncLater(pkg)
 		expect(Object.keys(pkg).length).toBeGreaterThan(1e6)
 	}, 150e4)
+
 	it('test job and load first', done => {
 		const pkg: Record<string, boolean> = {}
 		const j = job(
